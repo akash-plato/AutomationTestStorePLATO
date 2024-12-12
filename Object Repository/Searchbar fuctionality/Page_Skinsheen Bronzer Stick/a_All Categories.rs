ef2a54b0f9deecbcc2133903cbbaecf0c51cff0d
FrancisArquette <francis.arquette@platotech.com>
2024-12-12 16:15:46 -0500

    week 3 day 4

---
+++ Object Repository/Searchbar fuctionality/Page_Skinsheen Bronzer Stick/a_All Categories.rs
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_All Categories</name>
+   <tag></tag>
+   <elementGuidId>5e880c02-ecdb-4e5f-8806-8fbf434b6c46</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='category_0']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#category_0</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>f0165fc7-3447-44a9-8d59-d990678e13c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>category_0</value>
+      <webElementGuid>f504001e-9236-4832-8310-4cd0635803ed</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>All Categories</value>
+      <webElementGuid>51fcdce7-5ed6-4bc9-93f9-8036a6f8c3cd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;category_0&quot;)</value>
+      <webElementGuid>cf3e6650-fe6d-41ac-a80f-af03de7ad03a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@id='category_0']</value>
+      <webElementGuid>66f18383-f3fb-4baf-94dc-d8e01233353a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//ul[@id='search-category']/li[3]/a</value>
+      <webElementGuid>1ec76f85-0335-42f7-9dd9-fb9dc014de4f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'All Categories')])[2]</value>
+      <webElementGuid>1dcc740f-85d8-4a6c-8133-8a781dfe15ec</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='All Categories'])[1]/following::a[1]</value>
+      <webElementGuid>d2d83713-838d-42f2-9a1e-2e2c57b9b281</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Checkout'])[1]/following::a[2]</value>
+      <webElementGuid>0411a4c3-f0dd-48a5-9a33-579a3183a8e7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apparel &amp; accessories'])[1]/preceding::a[1]</value>
+      <webElementGuid>c1ee64c2-893c-4490-8aa3-52af16f69a6d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skincare'])[1]/preceding::a[2]</value>
+      <webElementGuid>c71c5169-7726-41d6-80c3-aa1e126fba72</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div/ul/li[3]/a</value>
+      <webElementGuid>34b3a600-79e3-408d-b223-32a933d29e6b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@id = 'category_0' and (text() = 'All Categories' or . = 'All Categories')]</value>
+      <webElementGuid>843aeff0-fbb6-4d53-af5b-6e13f0099e43</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
